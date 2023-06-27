ea518e64017c86d2d70984b472e4fc803920caa5
subhanprime <subhan25902@gmail.com>
2023-06-27 14:48:05 +0500

    all user functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Headers from "./components/headers";
+import { Route, Routes } from "react-router-dom";
+import Login from "./layouts/login/login";
+import Register from "./layouts/register/register";
+import LandingPage from "./layouts/landingPage/landingPage";
+import CheckHeader from "./components/checkHeader";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Headers /> */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route element={<CheckHeader />}>
+          <Route path="/" element={<LandingPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
