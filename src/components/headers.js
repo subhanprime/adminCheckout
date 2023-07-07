d5c246e237da930b3bd33d644d56d510d58bf0ae
subhanprime <subhan25902@gmail.com>
2023-07-07 15:53:31 +0500

    changes

--- src/components/headers.js
+++ src/components/headers.js
@@ -1,7 +1,208 @@
 import { Box, Typography } from "@mui/material";
 import logo from "../assets/svg/logo.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
+import WysiwygIcon from '@mui/icons-material/Wysiwyg';
+import ReportIcon from '@mui/icons-material/Report';
+import PersonOffIcon from '@mui/icons-material/PersonOff';
+import GavelIcon from '@mui/icons-material/Gavel';
+import PolicyIcon from '@mui/icons-material/Policy';
 const Headers = () => {
+  const [shortNav, setShortNav] = useState(false);
+
+  if (!shortNav) {
+    return (
+      <>
+        <Box
+          sx={{
+            height: "100%",
+            minWidth: "80px",
+            background: "#69C3F9",
+            boxShadow: "0 0 8px rgba(0,0,0,0.1)",
+            borderRadius: "7px",
+          }}
+        >
+          <Box p={1}>
+            <NavLink to="/">
+              <img src={logo} width="60px" />
+            </NavLink>
+          </Box>
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              fontSize: "18px",
+              cursor: "pointer",
+            }}
+            onClick={() => setShortNav(true)}
+          >
+            {/* <NavLink
+              to="/user"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#F24C3D" : "#fff",
+              })}
+              >
+              </NavLink> */}
+            <MenuIcon fontSize="large" />
+          </Box>
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <NavLink
+              to="/user"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#EB3A18" : "#fff",
+                // background: isActive ? "#7600dc" : "#f0f0f0",
+                // width: "100%",
+                // display
+              })}
+            >
+              <PersonIcon fontSize="large" />
+            </NavLink>
+          </Box>
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <NavLink
+              to="/post"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#EB3A18" : "#fff",
+                // background: isActive ? "#7600dc" : "#f0f0f0",
+                // width: "100%",
+                // display
+              })}
+            >
+              <WysiwygIcon fontSize="large" />
+            </NavLink>
+          </Box>
+
+
+
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <NavLink
+              to="/report"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#EB3A18" : "#fff",
+                // background: isActive ? "#7600dc" : "#f0f0f0",
+                // width: "100%",
+                // display
+              })}
+            >
+              <ReportIcon fontSize="large" />
+            </NavLink>
+          </Box>
+
+
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <NavLink
+              to="/userReport"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#EB3A18" : "#fff",
+                // background: isActive ? "#7600dc" : "#f0f0f0",
+                // width: "100%",
+                // display
+              })}
+            >
+              <PersonOffIcon fontSize="large" />
+            </NavLink>
+          </Box>
+
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <NavLink
+              to="/termsConditions"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#EB3A18" : "#fff",
+                // background: isActive ? "#7600dc" : "#f0f0f0",
+                // width: "100%",
+                // display
+              })}
+            >
+              <GavelIcon fontSize="large" />
+            </NavLink>
+          </Box>
+
+
+
+          <Box
+            mb={1}
+            sx={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+          >
+            <NavLink
+              to="/privacyPolicy"
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                color: isActive ? "#EB3A18" : "#fff",
+                // background: isActive ? "#7600dc" : "#f0f0f0",
+                // width: "100%",
+                // display
+              })}
+            >
+              <PolicyIcon fontSize="large" />
+            </NavLink>
+          </Box>
+        </Box>
+      </>
+    );
+  }
+
   return (
     <>
       <Box
@@ -46,14 +247,21 @@ const Headers = () => {
               <Box
                 mb={2}
                 sx={{
-                  cursor: "pointer",
                   display: "flex",
-                  justifyContent: "center",
+                  justifyContent: "space-between",
                 }}
               >
                 <NavLink to="/">
                   <img src={logo} width="100px" />
                 </NavLink>
+                <Box>
+                  <Typography
+                    onClick={() => setShortNav(false)}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <MenuOpenIcon />
+                  </Typography>
+                </Box>
               </Box>
             </Box>
             <Box>
@@ -83,8 +291,8 @@ const Headers = () => {
                       style={({ isActive }) => ({
                         textDecoration: "none",
                         color: isActive ? "#EB3A18" : "#fff",
-                        background: isActive ? "#7600dc" : "#f0f0f0",
-                        width:"100%"
+                        // background: isActive ? "#7600dc" : "#f0f0f0",
+                        width: "100%",
                       })}
                     >
                       User
